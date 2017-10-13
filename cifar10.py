5a0402fe8a74f3fc0f3adabc1b885e853d0b8349
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2017-10-13 22:34:31 +0530

    CNN using keras

--- cifar10.py
+++ cifar10.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 23:55:51 2017
+
+@author: hduser
+"""
+
 import numpy as np
 from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras.datasets import cifar10
 from keras.constraints import max_norm
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 from keras.utils import np_utils
 from keras import backend as K
+from time import time
 K.set_image_dim_ordering("th")
 
 np.random.seed(7)
@@ -40,15 +49,25 @@ model.add(Dense(512, activation='relu', kernel_constraint=max_norm(3)))
 model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 
-epochs = 50
-lrate = 0.1
-decay = 0.1/epochs
-sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+epochs = 60
+lrate = 0.0001
+decay = 0.01/epochs
+adam = Adam(lr=lrate, decay=decay, epsilon=1e-08)
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 print(model.summary())
 
+
+model_start_time = time()
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
+model_finish = time()
+
+
+# 9.92 hrs
+print("Total time taken :{0} hours".format((model_finish - model_start_time)/60/60))
+
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
+# 75.70
 print("Accuracy: %.2f%%" % (scores[1]*100))
 
+model.save("cnn_cifar10.pkl")
